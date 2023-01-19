a47b0b80da81892dd2f54c11d73f059d8ec18bd1
chpancrate <chpancrate@gmail.com>
2023-01-19 11:58:33 +0100

    demo mode added and readme file rework

--- README.md
+++ README.md
@@ -3,19 +3,45 @@
 ## Summary
 This project scraps the data from the website  http://books.toscrape.com/index.html.
 
-The books data are stored in a 'data/' directory in the working directory.
+The books data are stored in a 'data/' directory under the working directory.
 They are stored by books category in 'category'.csv files.
 
-The image files are stored in a 'images/' directory in the vorking directory. The image file name matches the book title.
+The image files are stored in 'images/category/' directories under the working directory. The image file name matches the book title.
 
 ## Requirements
-The modules needed for the scripts are in requirements.txt. In order to install them use the command :
+These scripts run with Python 3.11.1
+
+To install python you can download it here : https://www.python.org/downloads/
+
+If you are new to Python you can find information here : https://www.python.org/about/gettingstarted/ 
+
+It is better to run the scripts in a virtual environment. You can find information on virtual envrionments here : https://docs.python.org/3/library/venv.html 
+
+Once in your virtual environment, the following modules need to be installed :
+- bs4            : 0.0.1
+- beautifulsoup4 : 4.11.1
+- python-slugify : 7.0.0
+- requests       : 2.28.2
+
+All the modules needed for the scripts are in requirements.txt. A quick way to install them is to run the command below in a python terminal:
 ```
 pip install -r requirements.txt
 ```
+
 ## How to run
-You need to have python installed on your computer in order to run the script. Copy extract_data.py in the directory where you want the data to be stored and run the command : 
+
+In order to run it, clone the following repository in the directory where you want the data to be stored : https://github.com/chpancrate/ocrpy_project2
+
+### Normal mode
+To run the script in normal mode use the command : 
 ```
-python extract_data.py
+python ./extract_data.py
 ```
-The 'data/' and 'images/' directories will be created automatically if they do not exist.
+The script will retrieve all the data from the website. The 'data/' and 'images/' directories will be created automatically if they do not exist.
+
+### Demo mode
+There is a demo mode that will retrieve only the 5 first categories. To run it use the command : 
+```
+python ./extract_data.py demo
+```
+
--- extract_data.py
+++ extract_data.py
@@ -17,13 +17,14 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import os
+import sys
 from slugify import slugify
 from extract_functions import image_download, scrap_book, scrap_category, open_category_file
 
-if __name__ == "__main__":
 
-    print("extract script started")
-    # define the website to be scraped
+def main():
+    
+     # define the website to be scraped
     url = "http://books.toscrape.com/index.html"
  
     page = requests.get(url)
@@ -36,7 +37,11 @@ if __name__ == "__main__":
         # get rid of the first element as it is not a category
         categories.pop(0)
 
-        for category in categories:
+        if len(sys.argv) == 1:
+
+            print("extract script normal mode started")
+
+            for category in categories:
 
                 # retrieve partial url 
                 category_url=category.find("a")["href"]
@@ -60,11 +65,53 @@ if __name__ == "__main__":
                 #print("cat_url = ", cat_url)    
 
                 scrap_category(category_url, cat_url)
+                
+            print("extract script normal mode ended")
+ 
+        else:
+            if sys.argv[1] == "demo":
+                
+                print("extract script demo mode started")
+                
+                i = 1 # use to count the number of categories parsed, in demo mode stop at 5
+                
+                for category in categories:
+                
+                    if i < 6:
+                        # retrieve partial url 
+                        category_url=category.find("a")["href"]
+                        #print("category_url = ", category_url)
+
+                        # create the file name from the partial url 
+                        file_name = category_url.replace("catalogue/category/books/","")
+                        file_name = file_name.split("_")[0]
+                        file_name = file_name.lower()
+                        file_name = file_name.replace("-"," ")
+                        file_name = 'data/' + file_name + ".csv"
+                        #print(file_name)
+                        
+                        open_category_file(file_name)
+
+                        # create the full url from the partial url
+                        category_url="http://books.toscrape.com/" + category_url
+                        #print(category_url)
+
+                        cat_url = category_url.replace("index.html","") # truncated category url
+                        #print("cat_url = ", cat_url)    
+
+                        scrap_category(category_url, cat_url)
+                        i += 1
+                    else:
+                        print("extract script demo mode ended")
+                        return
+            else:
+                print("Bad arguments. Use without argument for normal mode or with the argument demo for the demo mode" )
+                return
     else:
         print("website url error")
+        return
 
-    print("extract script ended")
-
-        
-
+       
+if __name__ == "__main__":
+    main()
 
